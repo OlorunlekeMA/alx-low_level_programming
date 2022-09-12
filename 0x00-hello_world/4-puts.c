68c95250edf545f105d18798645010a556a07b41
OlorunlekeMA <arokoyoolorunleke@rocketmail.com>
2022-09-12 12:12:41 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - print Programming is like building a multilingual code
+ * Return: 0
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
