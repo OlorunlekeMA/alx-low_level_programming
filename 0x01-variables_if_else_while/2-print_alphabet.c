d1f29df06e618249bd3a99ecce41a98277e73518
OlorunlekeMA <arokoyoolorunleke@rocketmail.com>
2022-09-12 19:54:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - prints alphabet in lower case
+*Return: 0
+*/
+
+int main(void)
+{
+
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /* this is an acii code for new line */
+
+	return (0);
+
+}
